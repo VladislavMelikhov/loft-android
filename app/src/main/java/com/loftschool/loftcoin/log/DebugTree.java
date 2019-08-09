6d666a92d3698fdeb5c54830805107786831caee
Uladzislau Melikhau <umelikhau@iba.by>
2019-08-09 18:31:18 +0300

    Create Loft Application. Create Timber Debug Tree.

---
+++ app/src/main/java/com/loftschool/loftcoin/log/DebugTree.java
@@ -0,0 +1,29 @@
+package com.loftschool.loftcoin.log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+import timber.log.Timber;
+
+@SuppressWarnings("ThrowableNotThrown")
+public class DebugTree extends Timber.DebugTree {
+
+	@Override
+	protected void log(int priority, String tag, @NotNull String message, Throwable t) {
+		final StackTraceElement ste = new Throwable().fillInStackTrace().getStackTrace()[5];
+		super.log(
+			priority,
+			tag,
+			String.format(
+				Locale.getDefault(),
+				"[%s] %s(%s:%d): %s",
+				Thread.currentThread().getName(),
+				ste.getMethodName(),
+				ste.getFileName(),
+				ste.getLineNumber(),
+				message),
+			t
+		);
+	}
+}
