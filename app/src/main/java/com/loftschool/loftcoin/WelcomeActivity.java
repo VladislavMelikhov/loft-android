3130f4aadee6f3b4b17dfe4ddf65732c6b5fc15a
Uladzislau Melikhau <umelikhau@iba.by>
2019-08-14 21:25:21 +0300

    Move concrete welcome pages description out of WelcomePagesAdapter.

--- app/src/main/java/com/loftschool/loftcoin/WelcomeActivity.java
+++ app/src/main/java/com/loftschool/loftcoin/WelcomeActivity.java
@@ -5,12 +5,16 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class WelcomeActivity extends AppCompatActivity {
 
@@ -21,8 +25,15 @@ public final class WelcomeActivity extends AppCompatActivity {
 
 		final ViewPager vp_welcome_pager = findViewById(R.id.vp_welcome_pager);
 
-		vp_welcome_pager.setAdapter(new WelcomePagesAdapter(getLayoutInflater()));
-		this.<TabLayout>findViewById(R.id.tl_welcome_pages).setupWithViewPager(vp_welcome_pager);
+		vp_welcome_pager.setAdapter(
+			new WelcomePagesAdapter(
+				getWelcomePages(),
+				getLayoutInflater()
+			)
+		);
+
+		this.<TabLayout>findViewById(R.id.tl_welcome_pages)
+			.setupWithViewPager(vp_welcome_pager);
 
 		this.<AppCompatButton>findViewById(R.id.btn_start_working)
 			.setOnClickListener(view -> {
@@ -34,5 +45,25 @@ public final class WelcomeActivity extends AppCompatActivity {
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 				startActivity(intent);
 			});
+	}
+
+	@NonNull private List<WelcomePage> getWelcomePages() {
+		return Arrays.asList(
+			new WelcomePage(
+				R.drawable.welcome_page_1,
+				R.string.welcome_page_1_title,
+				R.string.welcome_page_1_subtitle
+			),
+			new WelcomePage(
+				R.drawable.welcome_page_2,
+				R.string.welcome_page_2_title,
+				R.string.welcome_page_2_subtitle
+			),
+			new WelcomePage(
+				R.drawable.welcome_page_3,
+				R.string.welcome_page_3_title,
+				R.string.welcome_page_3_subtitle
+			)
+		);
 	}
 }
--- app/src/main/java/com/loftschool/loftcoin/WelcomePagesAdapter.java
+++ app/src/main/java/com/loftschool/loftcoin/WelcomePagesAdapter.java
@@ -10,55 +10,42 @@ import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.util.List;
 import java.util.Objects;
 
 public final class WelcomePagesAdapter extends PagerAdapter {
 
-	private static final int[] IMAGES = {
-		R.drawable.welcome_page_1,
-		R.drawable.welcome_page_2,
-		R.drawable.welcome_page_3
-	};
-
-	private static final int[] TITLES = {
-		R.string.welcome_page_1_title,
-		R.string.welcome_page_2_title,
-		R.string.welcome_page_3_title
-	};
-
-	private static final int[] SUBTITLES = {
-		R.string.welcome_page_1_subtitle,
-		R.string.welcome_page_2_subtitle,
-		R.string.welcome_page_3_subtitle
-	};
-
 	private static final String PAGE_TITLE = "•";
 
-	private final LayoutInflater mInflater;
+	@NonNull private final List<WelcomePage> welcomePages;
+	@NonNull private final LayoutInflater inflater;
 
-	public WelcomePagesAdapter(@NonNull final LayoutInflater mInflater) {
-		this.mInflater = Objects.requireNonNull(mInflater);
+	public WelcomePagesAdapter(@NonNull final List<WelcomePage> welcomePages,
+	                           @NonNull final LayoutInflater inflater) {
+		this.welcomePages = Objects.requireNonNull(welcomePages);
+		this.inflater = Objects.requireNonNull(inflater);
 	}
 
 	@Override
 	public int getCount() {
-		return IMAGES.length;
+		return welcomePages.size();
 	}
 
 	@NonNull
 	@Override
 	public Object instantiateItem(@NonNull final ViewGroup container,
 	                              final int position) {
-		final View view = mInflater.inflate(R.layout.layout_welcome_page, container, false);
+		final View view = inflater.inflate(R.layout.layout_welcome_page, container, false);
+		final WelcomePage welcomePage = welcomePages.get(position);
 
 		container.addView(view, new ViewGroup.LayoutParams(
 			ViewGroup.LayoutParams.MATCH_PARENT,
 			ViewGroup.LayoutParams.MATCH_PARENT
 		));
 
-		view.<AppCompatImageView>findViewById(R.id.iv_welcome_image).setImageResource(IMAGES[position]);
-		view.<AppCompatTextView>findViewById(R.id.tv_welcome_title).setText(TITLES[position]);
-		view.<AppCompatTextView>findViewById(R.id.tv_welcome_subtitle).setText(SUBTITLES[position]);
+		view.<AppCompatImageView>findViewById(R.id.iv_welcome_image).setImageResource(welcomePage.getImageId());
+		view.<AppCompatTextView>findViewById(R.id.tv_welcome_title).setText(welcomePage.getTitleId());
+		view.<AppCompatTextView>findViewById(R.id.tv_welcome_subtitle).setText(welcomePage.getSubTitleId());
 		return view;
 	}
 
