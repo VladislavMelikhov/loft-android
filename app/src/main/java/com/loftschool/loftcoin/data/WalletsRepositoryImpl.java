3063c8e65f2684ee405a7df979bacb95a48305a6
Uladzislau Melikhau <umelikhau@iba.by>
2019-11-08 17:45:02 +0300

    Save wallet using Firestore.

--- app/src/main/java/com/loftschool/loftcoin/data/WalletsRepositoryImpl.java
+++ app/src/main/java/com/loftschool/loftcoin/data/WalletsRepositoryImpl.java
@@ -2,7 +2,11 @@ package com.loftschool.loftcoin.data;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.Query;
@@ -12,7 +16,9 @@ import com.loftschool.loftcoin.db.Transaction;
 import com.loftschool.loftcoin.db.Wallet;
 import com.loftschool.loftcoin.rx.RxSchedulers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 
@@ -94,6 +100,20 @@ public final class WalletsRepositoryImpl implements WalletsRepository {
 	@NonNull
 	@Override
 	public Completable saveWallet(@NonNull final Wallet wallet) {
-		return Completable.complete();
+		return Completable.create(emitter -> {
+			final Map<String, Object> data = new HashMap<>();
+			final CoinEntity coin = wallet.coin();
+			data.put("balance", wallet.balance1());
+			data.put("coinId", coin.id());
+			data.put("symbol", coin.symbol());
+			data.put("created", FieldValue.serverTimestamp());
+			firestore.collection("wallets").add(data)
+				.addOnSuccessListener(ioExecutor, documentReference -> {
+					if (!emitter.isDisposed()) {
+						emitter.onComplete();
+					}
+				})
+				.addOnFailureListener(ioExecutor, emitter::tryOnError);
+		});
 	}
 }
